ced8d934108e936820644294d64d58633f638043
Lorenzo <lollobaldo2000@gmail.com>
2019-02-19 18:53:13 +0000

    added comment highlighter setting

--- 1-10.hs
+++ 1-10.hs
@@ -1,7 +1,7 @@
 import Test.QuickCheck
 
 --------------------------------------------------
--- 1: Find the last element of a list.
+-- 1*: Find the last element of a list.
 f1 = myLast
 
 myLast :: [a] -> a
@@ -11,7 +11,7 @@ prop_f1 = f1 [1,2,3,4]     == 4
        && f1 ['x','y','z'] == 'z'
 
 --------------------------------------------------
--- 2: Find the last but one element of a list.
+-- 2*: Find the last but one element of a list.
 f2 = myButLast
 
 myButLast :: [a] -> a
@@ -21,7 +21,7 @@ prop_f2 = f2 [1,2,3,4]  == 3
        && f2 ['a'..'z'] == 'y'
 
 --------------------------------------------------
--- 3: Find the K'th element of a list.
+-- 3*: Find the K'th element of a list.
 --    The first element in the list is number 1
 f3 = elementAt
 
@@ -30,3 +30,49 @@ elementAt = (!!)
 
 prop_f3 = f3 [1,2,3]   2 == 2
        && f3 "haskell" 5 == 'e'
+
+--------------------------------------------------
+-- 4*: Find the number of elements of a list
+f4 = myLength
+
+myLength :: [a] -> Int
+myLength = length
+
+prop_f4 = f4 [123,456,789]   == 3
+       && f4 "Hello, world!" == 13
+
+--------------------------------------------------
+-- ciao 5: Reverse a list.
+f5 = myReverse
+
+myReverse :: [a] -> [a]
+myReverse = reverse
+
+prop_f5 = f5 "A man, a plan, a canal, panama!" == 
+                "!amanap ,lanac a ,nalp a ,nam A"
+       && f5 [1,2,3,4] == [4,3,2,1]
+
+--------------------------------------------------
+-- 6: Find out whether a list is a palindrome.
+--    A palindrome can be read forward or backward;
+--    e.g. (x a m a x)
+f6 = isPalindrome
+
+isPalindrome :: [a] -> Bool
+isPalindrome ls = ls == reverse ls
+
+prop_f6 = f6 [1,2,3]              == False
+       && f6 "madamimadam"        == True
+       && f6 [1,2,4,8,16,8,4,2,1] == True
+
+--------------------------------------------------
+-- 7: Reverse a list.
+f7 = Flatten a nested list structure
+
+myReverse :: [a] -> [a]
+myReverse = reverse
+
+prop_f5 = f5 "A man, a plan, a canal, panama!" == 
+                "!amanap ,lanac a ,nalp a ,nam A"
+       && f5 [1,2,3,4] == [4,3,2,1]
+
