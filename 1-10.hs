b97f67f4b3397d879481c54456698122aa9862a7
Lorenzo <lollobaldo2000@gmail.com>
2019-02-19 19:33:59 +0000

    1-9

--- 1-10.hs
+++ 1-10.hs
@@ -1,4 +1,5 @@
 import Test.QuickCheck
+import Data.List
 
 --------------------------------------------------
 -- *1: Find the last element of a list.
@@ -74,9 +75,53 @@ data NestedList a = Elem a | List [NestedList a] deriving (Eq, Show)
 flatten :: NestedList a -> [a]
 flatten (Elem a) = [a]
 flatten (List ls) = concatMap flatten ls
-
+-- TODO: Not sure, but the prop gives some type error on Eq
+--       being undefined. Manually checked them anyways.
 -- prop_f7 = f7 (Elem 5) == [5]
 --        && f7 (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
 --                 == [1,2,3,4,5]
 --        && f7 (List []) == []
 
+--------------------------------------------------
+-- **8: Eliminate consecutive duplicates of list elements.
+-- **    If a list contains repeated elements they should be
+-- **    replaced with a single copy of the element. The order
+-- **    of the elements should not be changed
+f8 = compress
+
+compress :: Eq a => [a] -> [a]
+compress (x:y:zs)
+    | x == y = compress (x:zs)
+    | otherwise = x : compress (y:zs)
+compress xs = xs
+
+prop_f8 = f8 "aaaabccaadeeee" == "abcade"
+
+--------------------------------------------------
+-- **9: Pack consecutive duplicates of list elements into
+-- **    sublists. If a list contains repeated elements they
+-- **    should be placed in separate sublists
+f9 = pack
+
+--imported Data.List
+
+pack :: Eq a => [a] -> [[a]]
+pack = group
+
+prop_f9 = f9 ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a',
+              'a', 'd', 'e', 'e', 'e', 'e'] ==
+              ["aaaa","b","cc","aa","d","eeee"]
+
+--------------------------------------------------
+-- **10: Run-length encoding of a list. Use the result of
+-- **     problem P09 to implement the so-colled run length
+-- **     encoding data compression method. Consecutive
+-- **     duplicates of elements are encoded as lists (N E)
+-- **     where N is the number of duplicates of the element E
+f10 ls = encode ls
+
+encode :: Eq a => [a] -> [(Int,a)]
+encode = undefined
+
+prop_f10 = f10 "aaaabccaadeeee" ==
+            [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
